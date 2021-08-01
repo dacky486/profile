f5ac49c190383bc3ab71ebb96bbab5a1ec1b7de6
dqwu <dacky486@gmail.com>
2021-08-01 22:27:52 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,163 @@
+(function () {
+  const projects = [
+    {
+      title: 'BubbleShooter',
+      image: './img/projects/bubbleshooter.png',
+      imageAlt: 'BubbleShooter Project Cover Photo',
+      description: 'In the game-based learning website, take bubble-defying games as an example, let students learn by playing games, use games and image memory to increase the effectiveness of learning, and make it easier for teachers to grasp the learning status of students',
+      badgeCaption: 'BubbleShooter',
+      links: {
+        github: 'https://github.com/dacky486/bubbleshooter'
+      },
+      accomplishments: [
+        'Built with Bootstrap, JavaScript,PHP',
+        'Using AJAX,Jquery',
+        'MySQL',
+      ]
+    }
+  ]
+
+  const nav = document.querySelector('nav')
+  const navHeight = nav.offsetHeight
+  const skillOffsetHeight = document.getElementById('skill').offsetTop
+  const projectOffsetHeight = document.getElementById('project').offsetTop
+  const actionBtn = document.querySelector('.fixed-action-btn a:first-of-type')
+  let skillsAnimated = false
+  let aboutCardPlaced = false
+  let projectPlaced = false
+  let learnMoreAnimated = false
+
+  // Handle animation end
+  function handleAnimationEnd(element, animations) {
+    element.classList.remove(...animations)
+  }
+
+  // Handle navbar animation
+  function animateNav() {
+    if (window.pageYOffset > navHeight) { return nav.classList.add('blue-grey', 'lighten-3', 'shadow') }
+    nav.classList.remove('blue-grey', 'lighten-3', 'shadow')
+  }
+
+  // Handle floating action button
+  function showFloatingActionButton() {
+    if (window.pageYOffset > navHeight) { return actionBtn.classList.remove('scale-out') }
+    actionBtn.classList.add('scale-out')
+  }
+
+  // Handle about cards animation
+  function animateAboutCards() {
+    if (window.pageYOffset <= navHeight) { return }
+    // switch status to placed
+    aboutCardPlaced = true
+    // get about section
+    const aboutSection = document.querySelector('.section-about .row')
+    // generate html for each blog post
+    blogPosts.forEach(post => {
+      // place post
+      aboutSection.innerHTML += `
+        <div class="col s12 m6 xl3">
+          <div class="card animated jackInTheBox slow">
+            <a href="${post.link}" target="_blank">
+              <div class="card-image" style="background-image: url(${post.image});">
+                <div class="overlay"></div>
+                <span class="card-title">
+                  ${post.name}
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+      `
+    })
+  }
+
+  // Handle skill section animation
+  function animateSkills() {
+    if (window.pageYOffset + window.innerHeight <= skillOffsetHeight) { return }
+    const firstSkillSection = document.getElementById('front-end-carousel-item')
+    const animations = ['animated', 'slideInRight']
+    skillsAnimated = true
+    firstSkillSection.classList.add(...animations)
+    firstSkillSection.addEventListener('animationend', () => handleAnimationEnd(event.target, animations))
+  }
+
+  // Handle learn more section animation
+  function animateLearnMore() {
+    const learnMoreOffsetHeight = document.getElementById('more-info').offsetTop
+    if (window.pageYOffset + window.innerHeight <= learnMoreOffsetHeight) { return }
+    const buttons = document.querySelectorAll('.learn-more-link a')
+    const animations = ['animated', 'bounceIn', 'slow']
+    learnMoreAnimated = true
+    buttons.forEach(button => button.classList.add(...animations))
+  }
+
+  // Generate icons of link
+  function getIconLinks(links) {
+    let icons = ``
+    if (links.github) {
+      icons += `<a href=${links.github} class="right btn-floating btn-small waves-effect waves-light teal" target="_blank"><i class="fab fa-github"></i></a>`
+    }
+    return icons
+  }
+
+  function getAccomplishments(accomplishments) {
+    return accomplishments.map(point => `<li><i class="fas fa-caret-right"></i>${point}</li>`).join('')
+  }
+
+  // Place all projects into project section
+  function placeProjects() {
+    // place projects when scroll to project section
+    if (window.pageYOffset + window.innerHeight <= projectOffsetHeight) { return }
+    // get elements
+    const projectSection = document.querySelector('.section-project .row')
+    // switch status to placed
+    projectPlaced = true
+    // generate html for each project
+    projects.forEach(project => {
+      // Get all icon links
+      const icons = getIconLinks(project.links)
+      // Gather all accomplishments
+      const accomplishments = getAccomplishments(project.accomplishments)
+
+      projectSection.innerHTML += `
+        <div class="col s12 m6 animated flipInX">
+          <div class="card sticky-action hoverable">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="responsive-img activator" src=${project.image}
+                alt="${project.image} Project Cover Photo">
+              <div class="overlay"></div>
+              <span class="card-title activator">${project.title}</span>
+            </div>
+            <div class="card-action">
+              <p class="activator truncate"><span class="new badge right activator"
+                data-badge-caption="${project.badgeCaption}"></span>${project.title}</p>
+            </div>
+            <div class="card-reveal">
+              <div class="overlay"></div>
+              <span class="card-title white-text">Accomplishments<i class="material-icons right">close</i></span>
+              <ul class="white-text">
+                ${accomplishments}
+              </ul>
+              <div id="card-reveal-icons">
+                ${icons}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col m5 hide-on-med-and-down offset-m1 valign-wrapper">
+          <h5 class="blue-grey-text text-darken-1">${project.title}</h5>
+          <span class="blue-grey-text text-lighten-1">${project.description}</span>
+        </div>
+      `
+    })
+  }
+
+  window.addEventListener('scroll', () => {
+    animateNav()
+    showFloatingActionButton()
+    if (!aboutCardPlaced) { animateAboutCards() }
+    if (!skillsAnimated) { animateSkills() }
+    if (!projectPlaced) { placeProjects() }
+    if (!learnMoreAnimated && projectPlaced) { animateLearnMore() }
+  })
+})()
